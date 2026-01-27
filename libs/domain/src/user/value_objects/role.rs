f39165fc4e6896a5af710240e30f2ddc70ee464b
Flip451 <flipflap451@gmail.com>
2026-01-28 01:01:49 +0900

    test: #42 UserRole のテスト

--- libs/domain/src/user/value_objects/role.rs
+++ libs/domain/src/user/value_objects/role.rs
@@ -14,6 +14,7 @@ pub enum UserRole {
 }
 
 #[derive(Debug, PartialEq, Eq, Display, EnumString, IntoStaticStr)]
+#[strum(serialize_all = "snake_case")]
 enum UserRoleKind {
     Admin,
     User,
@@ -39,5 +40,39 @@ impl TryFrom<&str> for UserRole {
 
     fn try_from(value: &str) -> Result<Self, Self::Error> {
         UserRole::from_str(value)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use rstest::rstest;
+
+    use super::*;
+
+    #[rstest]
+    #[case("admin", UserRole::Admin)]
+    #[case("user", UserRole::User)]
+    fn test_user_role_from_str_success(#[case] input: &str, #[case] expected: UserRole) {
+        let result = UserRole::from_str(input).unwrap();
+        assert_eq!(result, expected);
+    }
+
+    #[rstest]
+    #[case("invalid_role", UserReconstructionError::InvalidRole("invalid_role".to_string()))]
+    #[case("", UserReconstructionError::InvalidRole("".to_string()))]
+    fn test_user_role_from_str_failure(
+        #[case] input: &str,
+        #[case] expected: UserReconstructionError,
+    ) {
+        let result = UserRole::from_str(input);
+        assert_eq!(result, Err(expected));
+    }
+
+    #[rstest]
+    #[case(UserRole::Admin, "admin")]
+    #[case(UserRole::User, "user")]
+    fn test_user_role_to_string(#[case] role: UserRole, #[case] expected: &str) {
+        let role_str = role.to_string();
+        assert_eq!(role_str, expected);
     }
 }
