219563fe11f6f27b468d5609c529670b441225fe
Flip451 <flipflap451@gmail.com>
2026-02-14 19:58:09 +0900

    fix: #103 コメント修正

--- libs/domain/src/auth/policies/find_user_by_id.rs
+++ libs/domain/src/auth/policies/find_user_by_id.rs
@@ -23,10 +23,10 @@ impl<'a> Policy<'a> for FindUserByIdPolicy {
         let target_id = self.0.target_id;
 
         match ctx.actor_role {
-            UserRole::Admin => Ok(()), // 管理者は全ての詳細プロフィールを閲覧可能
+            UserRole::Admin => Ok(()), // 管理者は全てのユーザーを検索可能
             UserRole::User => {
                 if ctx.actor_id == target_id {
-                    Ok(()) // 自分自身の詳細プロフィールは閲覧可能
+                    Ok(()) // 自分自身は検索可能
                 } else {
                     Err(AuthorizationError::Forbidden) // その他のケースは拒否
                 }
